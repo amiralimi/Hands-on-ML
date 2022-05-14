95bd49ba4fa07bbd7a2420c6719f004746b08813
Amir Ali Mirian <amirali.mirian@gmail.com>
2022-05-15 00:11:50 +0430

    Most parts of chapter 17 is done. Variational Autoencoders and Progressive Growing of Gans is left.

--- utils/plots.py
+++ utils/plots.py
@@ -48,3 +48,15 @@ def plot_digits(instances, images_per_row=10, size=28, **options):
     image = np.concatenate(row_images, axis=0)
     plt.imshow(image, cmap=mpl.cm.binary, **options)
     plt.axis("off")
+
+
+def plot_multiple_images(images, n_cols=None):
+    n_cols = n_cols or len(images)
+    n_rows = (len(images) - 1) // n_cols + 1
+    if images.shape[-1] == 1:
+        images = np.squeeze(images, axis=-1)
+    plt.figure(figsize=(n_cols, n_rows))
+    for index, image in enumerate(images):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(image, cmap="binary")
+        plt.axis("off")
